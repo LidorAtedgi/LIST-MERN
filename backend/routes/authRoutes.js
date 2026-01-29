1bf233651cfdcf016cc87888123a559e819a32fb
LidorAtedgi <lidor123302@gmail.com>
2026-01-29 12:34:53 +0200

    Update authRoutes.js

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import {signup, login, logout,getMe,googleCallback} from "../controller/authController.js";
+import {signup, login, logout,getMe,googleCallback,deleteMe} from "../controller/authController.js";
 import protectRoute from "../middleware/protectRoute.js"
 import passport from "passport";
 
@@ -12,7 +12,7 @@ router.post("/login",login)
 router.post("/logout",logout)
 
 router.get("/me", protectRoute, getMe);
-
+router.delete("/me", protectRoute, deleteMe);
 router.get("/google", passport.authenticate("google", { scope:["profile"] }));
 
 router.get("/google/callback",
